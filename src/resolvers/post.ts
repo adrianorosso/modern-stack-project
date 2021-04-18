98adccb4481507dfef471b95e9bbb2ce5e89b436
Adriano F. Rosso <rosso.af@gmail.com>
2021-04-18 10:55:07 -0300

    add update post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -152,18 +152,27 @@ export class PostResolver {
   }
 
   @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
   async updatePost(
-    @Arg("id") id: number,
-    @Arg("title", { nullable: true }) title: string
+    @Arg("id", () => Int) id: number,
+    @Arg("title") title: string,
+    @Arg("text") text: string,
+    @Ctx() { req }: MyContext
   ): Promise<Post | null> {
     const post = await Post.findOne(id);
-    if (!post) {
-      return null;
-    }
-    if (typeof title !== "undefined") {
-      await Post.update({ id }, { title });
-    }
-    return post;
+
+    const result = await getConnection()
+      .createQueryBuilder()
+      .update(Post)
+      .set({ title: title, text: text })
+      .where("id = :id and creatorId = :creatorId ", {
+        id: post?.id,
+        creatorId: req.session.userId,
+      })
+      .returning("*")
+      .execute();
+
+    return result.raw[0];
   }
 
   @Mutation(() => Boolean)
